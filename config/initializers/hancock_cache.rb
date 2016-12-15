3463e899025f057c750b35b7cb17e805cb0d0f8a
Alexander Kiseliev <i43ack@gmail.com>
2016-12-15 08:34:39 +0300

    rails_admin actions fix

--- config/initializers/hancock_cache.rb
+++ config/initializers/hancock_cache.rb
@@ -1,4 +1,4 @@
 Hancock.rails_admin_configure do |config|
   config.action_visible_for :hancock_cache_clear, 'Hancock::Cache::Fragment'
-  config.action_visible_for :hancock_touch, -> { true }
+  config.action_visible_for :hancock_touch, Proc.new { false }
 end
