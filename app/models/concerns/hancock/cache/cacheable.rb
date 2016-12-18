1f1daf2b9f85d51755b4fac629870f660f2abac3
Alexander Kiseliev <i43ack@gmail.com>
2016-12-18 13:47:49 +0300

    conditional cache_keys

--- app/models/concerns/hancock/cache/cacheable.rb
+++ app/models/concerns/hancock/cache/cacheable.rb
@@ -28,10 +28,52 @@ if Hancock.mongoid?
         self.set_default_cache_keys(strategy) and self.save
       end
 
+      def conditional_cache_keys
+        [{}]
+      end
+      def selected_conditional_cache_keys
+        conditional_cache_keys and conditional_cache_keys.select { |cache_key|
+          cond_if, cond_unless = cache_key[:if], cache_key[:unless]
+
+          if cond_if
+            if cond_if.is_a?(Proc)
+              cond_if = !!cond_if.call
+            elsif cond_if.is_a?(String)
+              cond_if = !!self.instance_eval(cond_if)
+            else
+              cond_if = !!cond_if
+            end
+          else
+            cond_if = true
+          end
+
+          if cond_unless
+            if cond_unless.is_a?(Proc)
+              cond_unless = !cond_unless.call
+            elsif cond_unless.is_a?(String)
+              cond_unless = !self.instance_eval(cond_unless)
+            else
+              cond_unless = !cond_unless
+            end
+          else
+            cond_unless = true
+          end
+
+          cond_if and cond_unless
+        } or []
+      end
+
+      def all_cache_keys
+        return @all_cache_keys if @all_cache_keys
+        @all_cache_keys = cache_keys || []
+        @all_cache_keys += selected_conditional_cache_keys.map { |k| k[:name] }
+        @all_cache_keys
+      end
+
 
       def cache_keys
         return @cache_keys if @cache_keys
-        
+
         def <<(_keys)
           cache_keys_str = (cache_keys + _keys).select { |k|
             k and !k.strip.blank?
@@ -60,9 +102,12 @@ if Hancock.mongoid?
       after_destroy :clear_cache
       def clear_cache
         if perform_caching
-          cache_keys and cache_keys.is_a?(Array) and cache_keys.each do |k|
+          # cache_keys and cache_keys.is_a?(Array) and cache_keys.each do |k|
+          all_cache_keys and all_cache_keys.is_a?(Array) and all_cache_keys.each do |k|
             Rails.cache.delete(k)
           end
+          # @cache_keys = nil
+          @all_cache_keys = nil
         end
       end
 
