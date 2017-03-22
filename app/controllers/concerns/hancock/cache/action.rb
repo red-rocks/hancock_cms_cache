892d6d203983909f73fedee5ce73fc65858323fc
Alexander Kiseliev <i43ack@gmail.com>
2017-03-22 14:05:16 +0300

    hancock_stale; @vurtual_path as default hancock_cache_fragment key

--- app/controllers/concerns/hancock/cache/action.rb
+++ app/controllers/concerns/hancock/cache/action.rb
@@ -17,6 +17,26 @@ module Hancock::Cache::Action
       @page_cache_obj = Hancock::Cache::Fragment.create_unless_exists(name: Hancock::Cache::Fragment.name_from_view(_name), desc: _desc)
     end
 
+    def hancock_stale?(obj, opts = {})
+      return if obj.nil?
+      if obj.is_a?(Hash)
+        return stale?(obj)
+      end
+      if obj
+        _last_time = nil
+        _methods = ([:u_at, :updated_at, :c_at, :created_at] + (opts[:last_modified_methods] || [])).uniq
+        _methods.each do |m|
+          _last_time ||= obj.send(m) if obj.respond_to?(m)
+          break if _last_time
+        end
+        _last_time ||= Time.new
+        opts.reverse_merge!(last_modified: _last_time, etag: obj, public: true, template: false)
+        return stale?(obj)
+      else
+        true
+      end
+    end
+
     def hancock_cache_fragment_stale?(name, opts = {})
       if name.is_a?(Hash)
         name, opts = name.delete(:name), name
@@ -25,7 +45,7 @@ module Hancock::Cache::Action
       frag = hancock_cache_fragments[Hancock::Cache::Fragment.name_from_view(name)]
       if frag
         opts.reverse_merge!(last_modified: frag.last_clear_time, etag: frag, public: true, template: false)
-        stale?(opts.compact)
+        hancock_stale?(frag, opts.compact)
       else
         true
       end
