e0013baf246a7fe90a207c9a5a9c1cd873cb9055
Alexander Kiseliev <i43ack@gmail.com>
2017-11-08 17:44:08 +0300

    action_visible_for fix

--- config/initializers/hancock_cache.rb
+++ config/initializers/hancock_cache.rb
@@ -4,5 +4,5 @@ Hancock.rails_admin_configure do |config|
   config.action_visible_for :hancock_cache_restore_snapshot, 'Hancock::Cache::Fragment'
   config.action_visible_for :hancock_cache_clear, 'Hancock::Cache::Fragment'
   config.action_visible_for :hancock_cache_graph, 'Hancock::Cache::Fragment'
-  config.action_visible_for :hancock_touch, Proc.new { false }
+  config.action_visible_for :hancock_touch, Proc.new { true }
 end
